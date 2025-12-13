3771f4bfcfe507c73d15fa8c7d5db20aa0e4a738
KaranJayakumar <karanjaykumar@gmail.com>
2025-12-14 03:54:23 +1100

    Swap in bun and add key to blogs

--- app/_components/blog/Blog.tsx
+++ app/_components/blog/Blog.tsx
@@ -1,28 +1,34 @@
 "use server"
 import { promises as fs } from 'fs';
+import { useRouter } from 'next/router';
 import path from 'path'
 
 interface Blog{
+  key : string
   title : string
   description : string
   content : string
 }
 
 interface ListItemProps{
-  title : string
-  description : string
+  blog : Blog
 }
-export const BlogListItem = async ({title, description} : ListItemProps) => {
+export const BlogListItem = async ({ blog } : ListItemProps) => {
+  const openBlog = () => {
+    window.open(`/blogs/${blog.key}`)
+
+  }
   return (
     <div>
-      <div className="font-bold flex-col py-[10px]" id="blog-title">
-        <p className="pb-[4px]">{title}</p>
+      <div className="font-bold flex-col py-[10px] cursor-pointer" 
+        id="blog-title" onClick={() => openBlog()}>
+        <p className="pb-[4px]">{blog.title}</p>
       </div>
         <div className="font-normal flex-col text-sm" id="blog-list">
           <p>
-            <a href="#" className="underline hover:text-gray-600">
-            {description}
-            </a>
+            <p className="text-gray-600">
+            {blog.description}
+            </p>
           </p>
         </div>
     </div>
@@ -34,7 +40,7 @@ export const BlogList = async () => {
   return (
     blogs.map((blog, index) => {
         return (
-          <BlogListItem key={index} title ={blog.title} description={blog.description} />
+          <BlogListItem key={index} blog={blog}/>
         )
       }
     )
@@ -43,14 +49,15 @@ export const BlogList = async () => {
 
 async function loadBlogs() : Promise<Blog[]> {
   try{
-    const files = await fs.readdir(process.cwd() + '/app/static/blogs')
+    const files = await fs.readdir(process.cwd() + '/static/blogs')
     const results = []
     for(let i = 0; i < files.length; i++){
       const file = files[i]
       if(path.extname(file) == ".json"){
-         const fileContent = await fs.readFile(process.cwd() + '/app/static/blogs/' + file, 'utf8');
+         const fileContent = await fs.readFile(process.cwd() + '/static/blogs/' + file, 'utf8');
          const blogData = JSON.parse(fileContent);
          const blog : Blog = {
+           key : blogData.key,
            title : blogData.title,
            description : blogData.description,
            content : blogData.content,

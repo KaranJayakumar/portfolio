45aa0e0ff8984853f67de1e25ac5a4f6774fd498
KaranJayakumar <karanjaykumar@gmail.com>
2025-12-14 17:29:26 +1100

    Improve the look of the nav

--- app/_components/layout/NavigationBar.tsx
+++ app/_components/layout/NavigationBar.tsx
@@ -1,14 +1,14 @@
 "use client"
 import Link from "next/link"
-import { usePathname } from "next/navigation"
+import "../../styles/index.css"
 
 export const NavigationBar = () => {
   return (
-      <div className="flex flex-row justify-between mb-16 ">
+      <div className="flex flex-row justify-between mb-16 text-sm font-semibold">
         <div>
           <NavItem title="Karan Jayakumar" path="/"/>
         </div>
-        <div className="flex flex-row gap-8">
+        <div className="flex flex-row h-fit text-sm underline underline-offset-4 gap-x-10">
           <NavItem title="Experience" path="/"/>
           <NavItem title="Blog" path="/blogs"/>
           <NavItem title="Contact" path="/"/>
@@ -20,10 +20,11 @@ export const NavigationBar = () => {
 interface NavItemProps {
   title : string
   path : string
+  className?: string
 }
 
-const NavItem = ({title, path} : NavItemProps) => {
+const NavItem = ({title, path, className} : NavItemProps) => {
   return (
-    <Link href={path}>{title}</Link>
+    <Link href={path} className={`rounded-md nav-item ${className}`}>{title}</Link>
   )
 }

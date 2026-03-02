41cf3080cd10607d14db2933828a3f50f083ab96
KaranJayakumar <karanjaykumar@gmail.com>
2026-03-02 20:10:39 +1100

    Remove resume link

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -4,7 +4,6 @@ export const Footer = () => {
         <p className="pb-[8px] gap-x-4 flex">
           <a href="https://www.linkedin.com/in/karan-jayakumar-v2048/" className="underline hover:text-gray-600">LinkedIn</a>
           <a href="https://github.com/KaranJayakumar" className="underline hover:text-gray-600">GitHub</a>
-          <a href="/resume.pdf" download className="underline hover:text-gray-600">Resume</a>
           <a href="https://calendar.app.google/4Vr8TfsNmLjUEPTc9" target="_blank" className="underline hover:text-gray-600">Chat</a>
         </p>
       </div>

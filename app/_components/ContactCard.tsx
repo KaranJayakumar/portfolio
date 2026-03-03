cb638f642fd1ed9c7f756a104e710fa4ab5f466c
KaranJayakumar <karanjaykumar@gmail.com>
2026-03-03 22:15:13 +1100

    Remove other resume link

--- app/_components/ContactCard.tsx
+++ app/_components/ContactCard.tsx
@@ -8,7 +8,6 @@ export const ContactCard = () => {
           </div>
           <a href="https://www.linkedin.com/in/karan-jayakumar-v2048/" target="_blank" className="text-gray-600">LinkedIn</a>
           <a href="https://github.com/KaranJayakumar"  target="_blank" className="text-gray-600">GitHub</a>
-          <a href="/resume.pdf" download className="text-gray-600" target="_blank">Resume</a>
           <a href="https://calendar.app.google/4Vr8TfsNmLjUEPTc9" className="text-gray-600" target="_blank">Chat</a>
         </div>
       </div>

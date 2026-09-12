879e77f35325ea186661b00064d4d9babfc5d3f1
Karan Jayakumar <karanjaykumar@gmail.com>
2026-09-12 20:48:43 +1000

    Add view transition to main page

--- app/_components/layout/MainLayout.tsx
+++ app/_components/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from 'react';
+import { ReactNode, ViewTransition } from 'react';
 import { usePathname } from 'next/navigation';
 import { Header } from './Header';
 import { Footer } from './Footer';
@@ -12,7 +12,9 @@ export const MainLayout = ({ children } : LayoutProps) => {
     <div className="w-full items-center justify-center px-6 py-16">
       <Header/>
         <div className="flex min-h-svh w-full items-center justify-center">
-          {children}
+          <ViewTransition>
+            {children}
+          </ViewTransition>
         </div>
       <Footer/>
     </div>

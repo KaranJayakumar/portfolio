3ab3afa779624e3ee8eab18d48d121132d05dc25
Karan Jayakumar <karanjaykumar@gmail.com>
2026-09-12 20:38:20 +1000

    Center align footer

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-      <div className="font-normal flex-col text-sm pt-[40px] mt-[40px] border-t border-gray-200 pb-8" id="footer">
-        <p className="pb-[8px] gap-x-4 flex">
+      <div className="flex flex-col items-center text-center font-normal text-sm pt-[40px] mt-[40px] border-t border-gray-200 pb-8" id="footer">
+        <p className="flex justify-center gap-x-4 pb-[8px]">
           <a href="https://www.linkedin.com/in/karan-jayakumar-v2048/" className="underline hover:text-gray-600">LinkedIn</a>
           <a href="https://github.com/KaranJayakumar" className="underline hover:text-gray-600">GitHub</a>
           <a href="https://calendar.app.google/4Vr8TfsNmLjUEPTc9" target="_blank" className="underline hover:text-gray-600">Chat</a>
